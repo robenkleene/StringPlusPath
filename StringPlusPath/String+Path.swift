81da0ae500b832f2c48881e76db11e9fbb3931c2
Roben Kleene <contact@robenkleene.com>
2019-03-30 11:18:11 -0400

    Update project to Swift 5 (#18)

--- StringPlusPath/String+Path.swift
+++ StringPlusPath/String+Path.swift
@@ -9,41 +9,41 @@
 import Foundation
 
 public extension String {
-    public func appendingPathComponent(_ path: String) -> String {
+    func appendingPathComponent(_ path: String) -> String {
         // Writing this as `(self as NSString).appendingPathComponent(path)` causes a memory leak
         let string = self as NSString
         let result = string.appendingPathComponent(path)
         return result
     }
 
-    public func appendingPathExtension(_ ext: String) -> String? {
+    func appendingPathExtension(_ ext: String) -> String? {
         // Writing this as `(self as NSString).appendingPathExtension(ext)` causes a memory leak
         let string = self as NSString
         let result = string.appendingPathExtension(ext)
         return result
     }
 
-    public var deletingLastPathComponent: String {
+    var deletingLastPathComponent: String {
         return (self as NSString).deletingLastPathComponent
     }
 
-    public var lastPathComponent: String {
+    var lastPathComponent: String {
         return (self as NSString).lastPathComponent
     }
 
-    public var standardizingPath: String {
+    var standardizingPath: String {
         return (self as NSString).standardizingPath
     }
 
-    public var pathComponents: [String] {
+    var pathComponents: [String] {
         return (self as NSString).pathComponents
     }
 
-    public var pathExtension: String {
+    var pathExtension: String {
         return (self as NSString).pathExtension
     }
 
-    public var deletingPathExtension: String {
+    var deletingPathExtension: String {
         return (self as NSString).deletingPathExtension
     }
 }
